9cd46b20a575ba3ad0d5c8f73887a1a3efe906e5
marialuciadacunha26-svg <marialuciadacunha26@gmail.com>
2026-06-08 20:23:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Anarchy Legacy Bot online 💀");
-});
-
-app.listen(process.env.PORT || 3000);
+require("dotenv").config();
 
 const { Client, GatewayIntentBits } = require("discord.js");
-require("dotenv").config();
 
 const client = new Client({
   intents: [
@@ -18,12 +10,6 @@ const client = new Client({
   ]
 });
 
-// 💣 IMPORTANDO SEUS ARQUIVOS CORRETAMENTE
-const chaos = require("./chaos");
-const money = require("./money");
-const shop = require("./shop");
-const war = require("./war");
-
 client.once("ready", () => {
   console.log(`💀 Anarchy Legacy online como ${client.user.tag}`);
 });
@@ -31,27 +17,8 @@ client.once("ready", () => {
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
 
-  const args = message.content.split(" ");
-  const cmd = args.shift().toLowerCase();
-
-  if (cmd === "!ping") {
+  if (message.content === "!ping") {
     message.reply("🏓 Pong!");
-  }
-
-  if (cmd === "!chaos") {
-    chaos.execute(message);
-  }
-
-  if (cmd === "!money") {
-    money.execute(message, args);
-  }
-
-  if (cmd === "!shop") {
-    shop.execute(message);
-  }
-
-  if (cmd === "!war") {
-    war.execute(message);
   }
 });
 
