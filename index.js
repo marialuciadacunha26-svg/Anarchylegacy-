3ac42ca571c8b9297388f6064caf91b476fed11a
marialuciadacunha26-svg <marialuciadacunha26@gmail.com>
2026-06-08 20:09:27 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -18,8 +18,41 @@ const client = new Client({
   ]
 });
 
+// 💣 IMPORTANDO SEUS ARQUIVOS CORRETAMENTE
+const chaos = require("./chaos");
+const money = require("./money");
+const shop = require("./shop");
+const war = require("./war");
+
 client.once("ready", () => {
-  console.log(`💀 Bot online como ${client.user.tag}`);
+  console.log(`💀 Anarchy Legacy online como ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  const args = message.content.split(" ");
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd === "!ping") {
+    message.reply("🏓 Pong!");
+  }
+
+  if (cmd === "!chaos") {
+    chaos.execute(message);
+  }
+
+  if (cmd === "!money") {
+    money.execute(message, args);
+  }
+
+  if (cmd === "!shop") {
+    shop.execute(message);
+  }
+
+  if (cmd === "!war") {
+    war.execute(message);
+  }
 });
 
 client.login(process.env.TOKEN);
